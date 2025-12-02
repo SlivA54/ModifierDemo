3eafc61dbc608c2ad39bf8e200ddc4ac12163701
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-02 21:34:48 +0700

    Image

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,5 +45,13 @@ fun DemoScreen(modifier: Modifier = Modifier) {
         modifier = mymodifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun CustomImage(image: Int) {
+    Image(
+        painter = painterResource(image),
+        contentDescription = null
     )
 }
